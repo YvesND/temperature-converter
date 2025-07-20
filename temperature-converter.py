fa69756bde49700f729998c44c56587470c703f5
Yves <y.dortmann@gmail.com>
2025-07-21 01:10:56 +0200

    Add files via upload

---
+++ temperature-converter.py
@@ -0,0 +1,34 @@
+beenden = False
+
+while True:
+    einheit = str(input("Gibst du die Temperatur in Celsius oder Fahrenheit ein? (Schreibe C/F) "))
+    einheit = einheit.upper()
+    
+    if einheit == "C":
+        temperatur = float(input("Bitte Temperatur eingeben: "))
+        print(f"{temperatur} Grad Celsius entspricht {temperatur*1.8+32} Grad Fahrenheit.")
+    elif einheit == "F":
+        temperatur = float(input("Bitte Temperatur eingeben: "))
+        print(f"{temperatur} Grad Fahrenheit entspricht {(temperatur-32)/1.8} Grad Celsius.")
+    else:
+        print("Du hast keine gültige Eingabe gemacht. Bitte tippe C für Celsius oder F für Fahrenheit!")
+    
+    
+    while True:    
+        wiederholung = str(input("Willst du nochmal? (Schreibe J/N) "))
+        wiederholung = wiederholung.upper()
+        
+        if wiederholung not in ("J", "N"):
+            print("Du hast keine gültige Eingabe gemacht. Bitte tippe J für Ja oder N für Nein!")
+        
+        elif wiederholung == "J":
+            break
+        
+        elif wiederholung == "N":
+            print("Auf Wiedersehen!")
+            beenden = True
+            break 
+    
+    
+    if beenden:
+        break
